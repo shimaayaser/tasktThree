6c2f0c4faa023644488da26cf630f18c48278a77
shimaa <shimaayasser059@gmail.com>
2024-12-18 12:46:51 +0200

    new code

--- Assets/script/Assignment_27/ReferenceTest.cs
+++ Assets/script/Assignment_27/ReferenceTest.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace Assignment27
+{
 public class ReferenceTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
+        int a=6;
+        int b=4;
+        int newNum;
+
+        Debug.Log($"The number before call num method:{a}");
+        Num(a);
+        Debug.Log($"The number after call num method:{a}");
         
+        Debug.Log($"The number before call Numref method:{b}");
+        Numref( ref b);
+        Debug.Log($"The number after call Numref method:{b}");
+
+
+        Numout(out newNum);
+      
+
+        }
+    
+
+public void Num(int number){
+        number+=10;
+        Debug.Log($"The Number : {number}");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+public void Numref(ref int number)
+        {
+            number += 10;
+            Debug.Log($"The Reference Number : {number}");
+        }
+
+public void Numout(out int number)
+        {
+            number = 10;
+            Debug.Log($"The Out Number :{number}");
+        }
+
+
     }
 }
